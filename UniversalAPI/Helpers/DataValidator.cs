9e34c4e93a5864ef86e0c9dfc11a9ca134f1b995
Стас Высоцкий <ardenhide@gmail.com>
2022-03-08 18:24:49 +0300

    make change

---
+++ UniversalAPI/Helpers/DataValidator.cs
@@ -0,0 +1,101 @@
+﻿using Interfaces.DBModel;
+using Nethereum.Util;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversalApi.Helpers
+{
+    public static class DataValidator
+    {
+        public static dynamic GetRequest(Dictionary<string, dynamic> data)
+        {
+            if (data.ContainsKey("Request") || data.ContainsKey("request"))
+            {
+                dynamic requestName;
+                if (data.TryGetValue("Request", out requestName))
+                    return requestName;
+                if (data.TryGetValue("request", out requestName))
+                    return requestName;
+            }
+            return null;
+        }
+        public static dynamic GetId(Dictionary<string, dynamic> data)
+        {
+            if (data.ContainsKey("Id") || data.ContainsKey("id"))
+            {
+                dynamic id;
+                if (data.TryGetValue("Id", out id))
+                    return id;
+                if (data.TryGetValue("id", out id))
+                    return id;
+            }
+            return null;
+        }
+        public static dynamic GetAddress(Dictionary<string, dynamic> data)
+        {
+            if (data.ContainsKey("Address") || data.ContainsKey("address"))
+            {
+                dynamic address;
+                if (data.TryGetValue("Address", out address))
+                    return address;
+                if (data.TryGetValue("address", out address))
+                    return address;
+            }
+            return null;
+        }
+        public static dynamic GetOwner(Dictionary<string, dynamic> data)
+        {
+            if (data.ContainsKey("Owner") || data.ContainsKey("owner"))
+            {
+                dynamic owner;
+                if (data.TryGetValue("Owner", out owner))
+                    return owner;
+                if (data.TryGetValue("owner", out owner))
+                    return owner;
+            }
+            return null;
+        }
+
+        public static bool HasRequest(Dictionary<string, dynamic> data, out string tables)
+        {
+            return IsValidRequestName(GetRequest(data), out tables);
+        }
+        public static bool IsValidRequestName(string requestName, out string tables)
+        {
+            tables = string.Empty;
+            using DynamicDBContext context = DynamicDB.ConnectToDb();
+            var request = context.APIRequestList.FirstOrDefault(p => p.Request == requestName);
+            if (request != null && request.Tables != string.Empty)
+            {
+                tables = request.Tables;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CheckId(Dictionary<string, dynamic> data) =>
+            IsValidId((int?)GetId(data));
+        public static bool IsValidId(int? id)
+        {
+            if (id == null)
+                return false;
+            if (id < 0)
+                return false;
+
+            return true;
+        }
+
+        public static bool CheckAddress(Dictionary<string, dynamic> data) =>
+            IsValidAddress(GetAddress(data));
+        public static bool IsValidAddress(string address)
+        {
+            bool result = AddressExtensions.IsValidEthereumAddressHexFormat(address);
+            if (!result)
+                return false;
+            return true;
+        }
+
+        public static bool CheckOwner(Dictionary<string, dynamic> data) =>
+            IsValidAddress(GetOwner(data));
+    }
+}
--- UniversalAPI/Helpers/QueryCreator.cs
+++ UniversalAPI/Helpers/QueryCreator.cs
@@ -18,9 +18,9 @@ namespace UniversalApi.Helpers
             string tables;
             string columns;
             string joinCondition;
-            if (HasRequest(data, out tables))
+            if (DataValidator.HasRequest(data, out tables))
             {
-                var request = data["Request"];
+                var request = DataValidator.GetRequest(data);
                 columns = GetColumns(request);
                 joinCondition = GetJoinCondition(request);
                 data.Remove("Request");
@@ -31,9 +31,9 @@ namespace UniversalApi.Helpers
             }
             
 
-            if (!CheckId(data))
+            if (!DataValidator.CheckId(data))
                 return null;
-            if (!CheckAddress(data))
+            if (!DataValidator.CheckAddress(data))
                 return null;
 
             List<string> tablesName = GetTablesName(tables);
@@ -95,72 +95,6 @@ namespace UniversalApi.Helpers
                 $"WHERE {condition}";
 
             return commandQuery;
-        }
-
-        private static bool HasRequest(Dictionary<string, dynamic> data, out string tables)
-        {
-            tables = string.Empty;
-            if (data.ContainsKey("Request") || data.ContainsKey("request"))
-            {
-                dynamic requestName;
-                if (data.TryGetValue("Request", out requestName))
-                    return IsValidRequestName(requestName, out tables);
-                if (data.TryGetValue("request", out requestName))
-                    return IsValidRequestName(requestName, out tables);
-            }
-            return false;
-        }
-        private static bool CheckId(Dictionary<string, dynamic> data)
-        {
-            if (data.ContainsKey("Id") || data.ContainsKey("id"))
-            {
-                dynamic id;
-                if (data.TryGetValue("Id", out id))
-                    return IsValidId((int?)id);
-                if (data.TryGetValue("id", out id))
-                    return IsValidId((int?)id);
-            }
-            return true;
-        }
-        private static bool CheckAddress(Dictionary<string, dynamic> data)
-        {
-            if (data.ContainsKey("Address") || data.ContainsKey("address"))
-            {
-                dynamic address;
-                if (data.TryGetValue("Address", out address))
-                    return IsValidAddress((string)address);
-                if (data.TryGetValue("address", out address))
-                    return IsValidAddress((string)address);
-            }
-            return true;
-        }
-        private static bool IsValidRequestName(string requestName, out string tables)
-        {
-            tables = string.Empty;
-            using DynamicDBContext context = DynamicDB.ConnectToDb();
-            var request = context.APIRequestList.FirstOrDefault(p => p.Request == requestName);
-            if (request != null && request.Tables != string.Empty)
-            {
-                tables = request.Tables;
-                return true;
-            }
-            return false;
-        }
-        private static bool IsValidId(int? id)
-        {
-            if (id == null)
-                return false;
-            if (id < 0)
-                return false;
-
-            return true;
-        }
-        private static bool IsValidAddress(string address)
-        {
-            bool result = AddressExtensions.IsValidEthereumAddressHexFormat(address);
-            if (!result)
-                return false;
-            return true;
         }
 
         private static List<string> GetTablesName(string tables)
