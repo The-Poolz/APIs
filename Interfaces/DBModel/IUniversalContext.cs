778226fb2759d3890afd905afcfd1b513332185d
Стас Высоцкий <ardenhide@gmail.com>
2022-03-14 11:22:55 +0300

    added summary

--- Interfaces/DBModel/IUniversalContext.cs
+++ Interfaces/DBModel/IUniversalContext.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Interfaces.DBModel
 {
+    /// <summary>
+    /// Implement the interface in your context for the UniversalAPI to work.
+    /// </summary>
     public interface IUniversalContext
     {
         DbSet<APIRequestList> APIRequestList { get; }
