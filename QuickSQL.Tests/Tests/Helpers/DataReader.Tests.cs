d3bdd4c027d2bb38ba79e20fe3cb5b8be7e71766
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-04-19 17:16:17 +0300

    Update DataReader.Tests.cs

--- QuickSQL.Tests/Tests/Helpers/DataReader.Tests.cs
+++ QuickSQL.Tests/Tests/Helpers/DataReader.Tests.cs
@@ -19,7 +19,7 @@ namespace QuickSQL.Tests.Helpers
             var expected = "[{\"PoolId\":3,\"Rank\":\"3\",\"Owner\":\"0x3a31ee5557c9369c35573496555b1bc93553b553\",\"Amount\":\"250.02109769151781894\"}]";
             var commandQuery = QueryCreator.CreateCommandQuery(request);
             var context = MockContext.GetTestDataContext();
-            context.Database.SetConnectionString(@"Data Source=127.0.0.1;User id=root;Password=;Initial Catalog=QuickSQL.Test");
+            context.Database.SetConnectionString(@"Server=127.0.0.1;Database=QuickSQL.Test;Uid=root;Pwd=;");
             context.Database.OpenConnection();
 
             var result = DataReader.GetJsonData(commandQuery, context.Database.GetConnectionString());
