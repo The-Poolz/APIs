0f38e6c981f7f2d24a83f664cacb4b3fc7b276d5
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-04-25 19:56:33 +0300

    update expected value for tests

--- QuickSQL.Tests/Tests/Helpers/DataReader.Tests.cs
+++ QuickSQL.Tests/Tests/Helpers/DataReader.Tests.cs
@@ -13,7 +13,7 @@ namespace QuickSQL.Tests.Helpers
                 SelectedTable = "TokenBalances",
                 SelectedColumns = "Token, Owner, Amount"
             };
-            var expected = "[{\"Token\":\"ADH\",\"Owner\":\"0x1a01ee5577c9d69c35a77496565b1bc95588b521\",\"Amount\":\"400\"},{\"Token\":\"Poolz\",\"Owner\":\"0x2a01ee5557c9d69c35577496555b1bc95558b552\",\"Amount\":\"300\"},{\"Token\":\"ETH\",\"Owner\":\"0x3a31ee5557c9369c35573496555b1bc93553b553\",\"Amount\":\"200\"},{\"Token\":\"BTH\",\"Owner\":\"0x4a71ee5577c9d79c37577496555b1bc95558b554\",\"Amount\":\"100\"}]";
+            var expected = "[{\"Owner\": \"0x1a01ee5577c9d69c35a77496565b1bc95588b521\", \"Token\": \"ADH\", \"Amount\": \"400\"}, {\"Owner\": \"0x2a01ee5557c9d69c35577496555b1bc95558b552\", \"Token\": \"Poolz\", \"Amount\": \"300\"}, {\"Owner\": \"0x3a31ee5557c9369c35573496555b1bc93553b553\", \"Token\": \"ETH\", \"Amount\": \"200\"}, {\"Owner\": \"0x4a71ee5577c9d79c37577496555b1bc95558b554\", \"Token\": \"BTH\", \"Amount\": \"100\"}]";
             var commandQuery = QueryCreator.CreateCommandQuery(request);
             string connectionString = @$"server=127.0.0.1;user id=root;password=;database=QuickSQL.Test";
 
