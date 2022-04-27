76bc301279ad84d9d830a93d5125c8f55c1a9a48
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-04-27 16:22:51 +0300

    remove useless project

--- Interfaces/ConnectionString.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Interfaces
-{
-    public static class ConnectionString
-    {
-        public static string ConnectionToData = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UniversalApi.Data;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
