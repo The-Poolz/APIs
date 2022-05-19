ffd0f8f631246554a60ee5f507ab1b2a931e01f0
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-05-19 16:27:38 +0300

    test commit

--- QuickSQL/DataReader/BaseDataReader.cs
+++ QuickSQL/DataReader/BaseDataReader.cs
@@ -8,43 +8,17 @@ namespace QuickSQL.DataReader
         public string GetJsonData(string commandQuery, string connectionString)
         {
             string emptyJson = "[]";
-            StringBuilder jsonResult = new StringBuilder();
+            using var connection = CreateConnection(connectionString);
+            connection.Open();
+            var reader = CreateReader(commandQuery, connection);
 
-            System.Console.WriteLine("Start read Json data");
-            System.Console.WriteLine("Received SQL query: " + commandQuery);
-
-            using (var connection = CreateConnection(connectionString))
+            var jsonResult = new StringBuilder();
+            while (reader.Read())
             {
-                System.Console.WriteLine("Created connection: " + connection.State);
-                connection.Open();
-                System.Console.WriteLine("Connection opened: " + connection.State);
-
-                var reader = CreateReader(commandQuery, connection);
-                System.Console.WriteLine("Reader created, reader IsClosed: " + reader.IsClosed);
-
-                while (reader.Read())
-                {
-                    System.Console.WriteLine("Has reader more rows: " + reader.Read());
-                    jsonResult.Append(reader.GetValue(0).ToString());
-                    System.Console.WriteLine("Row result: " + jsonResult);
-                }
-                reader.Close();
-                System.Console.WriteLine("End reading.");
+                jsonResult.Append(reader.GetValue(0).ToString());
             }
-            System.Console.WriteLine("End connection.");
-
             if (string.IsNullOrEmpty(jsonResult.ToString()))
                 return emptyJson;
-
-            string cwRes = "BaseDataReader result: " + jsonResult;
-            int lengthCwRes = cwRes.Length;
-            System.Console.WriteLine(cwRes);
-            for (int i = 0; i < lengthCwRes; i++)
-            {
-                System.Console.Write('=');
-            }
-            System.Console.WriteLine("");
-
             return jsonResult.ToString();
         }
         public abstract DbConnection CreateConnection(string connectionString);
