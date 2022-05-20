7323c0656547d5db0f5775cbe2e6d6f7c1d105c8
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-05-20 14:51:20 +0300

    Create LocalConnection.cs

---
+++ QuickSQL.MicrosoftSqlServer.Tests/LocalConnection.cs
@@ -0,0 +1,7 @@
+namespace QuickSQL.Tests
+{
+    public static class LocalConnection
+    {
+        public const string MicrosoftSqlServerConnection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=UniversalApi.Example.Data;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
