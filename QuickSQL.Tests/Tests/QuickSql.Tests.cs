dd2302e7526c6ecfa8eb9e44b5dbf5fcb03fce6c
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-05-29 15:16:24 +0300

    added small test logging for QuickSqlTests

--- QuickSQL.Tests/Tests/QuickSql.Tests.cs
+++ QuickSQL.Tests/Tests/QuickSql.Tests.cs
@@ -1,5 +1,7 @@
 using Xunit;
 using System;
+using Serilog;
+using Xunit.Abstractions;
 using System.Globalization;
 using System.Collections.ObjectModel;
 
@@ -14,12 +16,34 @@ namespace QuickSQL.Tests
     /// <remarks>
     /// All tests with databese use the MySql provider.
     /// </remarks>
-    public static class QuickSqlTests
+    public class QuickSqlTests
     {
-        [Fact]
-        public static void InvokeRequest()
+        public QuickSqlTests(ITestOutputHelper output)
         {
-            string isTravisCi = Environment.GetEnvironmentVariable("IsTravisCI");
+            Log.Logger = new LoggerConfiguration()
+            // add the xunit test output sink to the serilog logger
+            // https://github.com/trbenning/serilog-sinks-xunit#serilog-sinks-xunit
+            .WriteTo.TestOutput(output)
+            .CreateLogger();
+        }
+
+        [Fact]
+        public void InvokeRequest()
+        {
+            // Test logging
+            TestLog log = new TestLog("InvokeRequest");
+            log.StartTest();
+
+            // Is TravisCI settings
+            bool isTravis = Convert.ToBoolean(Environment.GetEnvironmentVariable("IsTravisCI"), new CultureInfo("en-US"));
+            string connectionString;
+            if (Convert.ToBoolean(isTravis, new CultureInfo("en-US")))
+                connectionString = Environment.GetEnvironmentVariable("TravisCIMySqlConnection");
+            else
+                connectionString = LocalConnection.MySqlConnection;
+            log.WriteConnection(isTravis, connectionString);
+
+            // Arrange
             string expected = "[{\"Owner\": \"0x1a01ee5577c9d69c35a77496565b1bc95588b521\", \"Token\": \"ADH\", \"Amount\": \"400\"}]";
             var request = new Request(
                 "TokenBalances",
@@ -31,23 +55,21 @@ namespace QuickSQL.Tests
                 {
                     new Condition { ParamName = "Id", Operator = OperatorName.Equals, ParamValue = "1" }
                 });
-            string connectionString;
 
-            if (Convert.ToBoolean(isTravisCi, new CultureInfo("en-US")))
-                connectionString = Environment.GetEnvironmentVariable("TravisCIMySqlConnection");
-            else
-                connectionString = LocalConnection.MySqlConnection;
-
+            // Act
             var result = QuickSql.InvokeRequest(request, connectionString,
                 new MySqlDataReader(), new MySqlQueryCreator());
+            log.WriteResult(expected, result);
 
             Assert.NotNull(result);
             Assert.IsType<string>(result);
             Assert.Equal(expected, result);
+
+            log.EndTest();
         }
 
         [Fact]
-        public static void InvokeRequestWithoutConnectionString()
+        public void InvokeRequestWithoutConnectionString()
         {
             var request = new Request(
                 "TokenBalances",
@@ -68,7 +90,7 @@ namespace QuickSQL.Tests
         }
 
         [Fact]
-        public static void InvokeRequestInvalidConnectionString()
+        public void InvokeRequestInvalidConnectionString()
         {
             var request = new Request(
                 "TokenBalances",
@@ -93,7 +115,7 @@ namespace QuickSQL.Tests
         }
 
         [Fact]
-        public static void InvokeRequestInvalidProviders()
+        public void InvokeRequestInvalidProviders()
         {
             var request = new Request(
                 "TokenBalances",
