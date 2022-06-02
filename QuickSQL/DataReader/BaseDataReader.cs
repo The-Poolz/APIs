a0f35884b9e7be821719684214313e998b760f30
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-06-02 18:00:51 +0300

    Updated QuickSQL.csproj, remove useless DataReader code

--- QuickSQL/DataReader/BaseDataReader.cs
+++ QuickSQL/DataReader/BaseDataReader.cs
@@ -22,22 +22,6 @@ namespace QuickSQL.DataReader
             return jsonResult.ToString();
         }
 
-        public string GetJsonDataFromExpandoObject(string commandQuery, string connectionString)
-        {
-            string emptyJson = "[]";
-            using var connection = CreateConnection(connectionString);
-            connection.Open();
-            var reader = CreateReader(commandQuery, connection);
-
-            var jsonResult = new StringBuilder();
-            while (reader.Read())
-            {
-                jsonResult.Append(reader.GetValue(0).ToString());
-            }
-            if (string.IsNullOrEmpty(jsonResult.ToString()))
-                return emptyJson;
-            return jsonResult.ToString();
-        }
         public abstract DbConnection CreateConnection(string connectionString);
         public abstract DbDataReader CreateReader(string commandQuery, DbConnection connection);
     }
