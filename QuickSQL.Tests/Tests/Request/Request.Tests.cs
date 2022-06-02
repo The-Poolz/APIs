ea4aa505b2ed9924c28fb67e9c99747dae710ee2
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-06-02 21:19:21 +0300

    Update Request model

--- QuickSQL.Tests/Tests/Request/Request.Tests.cs
+++ QuickSQL.Tests/Tests/Request/Request.Tests.cs
@@ -7,15 +7,6 @@ namespace QuickSQL.Tests.Requests
     public static class RequestTests
     {
         [Fact]
-        public static void CreateRequest()
-        {
-            var request = new Request();
-
-            Assert.NotNull(request);
-            Assert.IsType<Request>(request);
-        }
-
-        [Fact]
         public static void CreateRequestWithParams()
         {
             var request = new Request(
