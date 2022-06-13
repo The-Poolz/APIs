d4495aa62968287bc32032eb0306b7dcb659a761
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-06-13 11:23:42 +0300

    added logic

--- QuickSQL/DataReader/BaseDataReader.cs
+++ QuickSQL/DataReader/BaseDataReader.cs
@@ -8,6 +8,7 @@ namespace QuickSQL.DataReader
         public string GetJsonData(string commandQuery, string connectionString)
         {
             string emptyJson = "[]";
+            int readCount = 0;
             using var connection = CreateConnection(connectionString);
             connection.Open();
             var reader = CreateReader(commandQuery, connection);
@@ -16,9 +17,17 @@ namespace QuickSQL.DataReader
             while (reader.Read())
             {
                 jsonResult.Append(reader.GetValue(0).ToString());
+                readCount++;
             }
             if (string.IsNullOrEmpty(jsonResult.ToString()))
                 return emptyJson;
+            if (readCount == 1)
+            {
+                jsonResult.ToString().Remove(
+                    jsonResult.ToString().IndexOf('['));
+                jsonResult.ToString().Remove(
+                    jsonResult.ToString().IndexOf(']'));
+            }
             return jsonResult.ToString();
         }
 
